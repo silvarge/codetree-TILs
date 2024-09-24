c91b1bf14eaa068ce2a7631222c6237126f7b71d
silvarge <140832682+silvarge@users.noreply.github.com>
2024-09-24 11:01:31 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "Total days in Year\n" << 365 << "\n" << "Circumference rate\n" << "3.1415926535";
+    return 0;
+}
